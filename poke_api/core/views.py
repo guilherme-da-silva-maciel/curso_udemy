3d0c51798bb6c974d437d0fdbf7f664d28296a1f
drakosilva <macielguilherme413@gmail.com>
2022-04-25 16:42:28 -0300

    comit da continuação do site

--- poke_api/core/views.py
+++ poke_api/core/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def home(request):
     return render(request,'pokemons/pages/home.html',context={'name' : 'darkrai'})
 
+def poke(request,id):
+    return render(request,'pokemons/pages/poke-view.html',context={'name' : 'darkrai'})
+
 # Create your views here.
